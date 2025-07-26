73f38fe5bd314b6272c51b223b77cbe7cd55c45b
hricky13 <hricky13@gmail.com>
2025-07-26 14:36:39 +0200

    Started working on data ingestion pipeline

---
+++ recipesitetraffic/components/data_ingestion.py
@@ -0,0 +1,86 @@
+import os
+import sys
+from dotenv import load_dotenv
+from recipesitetraffic.exception.exception import RecipeSiteTrafficException
+from recipesitetraffic.logging.logger import logging
+from recipesitetraffic.entity.config_entity import DataIngestionConfig
+from recipesitetraffic.entity.artifact_entity import DataIngestionArtifact
+
+import numpy as np
+import pandas as pd
+import pymongo
+from typing import List
+from sklearn.model_selection import train_test_split
+
+load_dotenv()
+
+MONGO_DB_URL = os.getenv("MONGO_DB_URL")
+
+class DataIngestion:
+    def __init__(self,data_ingestion_config:DataIngestionConfig):
+        try:
+            self.data_ingestion_config = data_ingestion_config
+                       
+        except Exception as e:
+            raise RecipeSiteTrafficException(e, sys)
+        
+    def export_collection_as_dataframe(self):
+        try:
+            database_name = self.data_ingestion_config.database_name
+            collection_name = self.data_ingestion_config.collection_name
+            self.mongo_client = pymongo.MongoClient(MONGO_DB_URL)
+            collection = self.mongo_client[database_name][collection_name]
+            
+            df = pd.DataFrame(list(collection.find()))
+            
+            if "_id" in df.columns.tolist():
+                df.drop(columns=['_id'], axis=1, inplace=True)
+            df.replace({"na":np.nan}, inplace=True)
+            
+            return df               
+        except Exception as e:
+            raise RecipeSiteTrafficException(e, sys)
+        
+    def export_data_to_feature_store(self, df: pd.DataFrame):
+        try:
+            feature_store_path = self.data_ingestion_config.feature_store_file_path
+            dir_path = os.path.dirname(feature_store_path)
+            
+            os.makedirs(dir_path, exist_ok=True)
+            df.to_csv(feature_store_path, index=False, header=True)
+            
+            return df
+            
+        except Exception as e:
+            raise RecipeSiteTrafficException(e, sys)
+        
+    def split_data_into_train_and_test(self, df: pd.DataFrame):
+        try:
+            train_df, test_df = train_test_split(df, test_size=self.data_ingestion_config.train_test_split_ratio, random_state=42)
+            dir_path = os.path.dirname(self.data_ingestion_config.training_file_path)
+            os.makedirs(dir_path, exist_ok=True)
+            train_df.to_csv(self.data_ingestion_config.training_file_path, index=False, header=True)
+            test_df.to_csv(self.data_ingestion_config.testing_file_path, index=False, header=True)
+
+
+        except Exception as e:
+            raise RecipeSiteTrafficException(e, sys)
+        
+    def initiate_data_ingestion(self):
+        try:
+            df = self.export_collection_as_dataframe()
+            df = self.export_data_to_feature_store()
+            
+            self.split_data_into_train_and_test()
+            data_ingestion_artifact = DataIngestionArtifact(
+                train_file_path=self.data_ingestion_config.training_file_path,
+                test_file_path=self.data_ingestion_config.testing_file_path
+            )
+            
+            return data_ingestion_artifact
+            
+            
+            
+            
+        except Exception as e:
+            raise RecipeSiteTrafficException(e, sys)
---
+++ recipesitetraffic/constants/constants.py
@@ -0,0 +1,26 @@
+import os
+import sys
+import numpy as np
+import pandas as pd
+
+"""
+Defining common constant variables for the training pipeline
+"""
+TARGET_COLUMN: str = "high_traffic"
+PIPELINE_NAME: str = "RecipeSiteTraffic"
+ARTIFACT_DIR: str = "artifacts"
+FILE_NAME: str = "recipe_site_traffic.csv"
+
+TRAIN_FILE_NAME: str = "train.csv"
+TEST_FILE_NAME: str = "test.csv"
+
+"""
+Data ingestion related constants start with DATA_INGESTION var name
+"""
+
+DATA_INGESTION_COLLECTION_NAME: str = "traffic_data"
+DATA_INGESTION_DATABASE_NAME: str = "recipe_db"
+DATA_INGESTION_DIR_NAME: str = "data_ingestion"
+DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
+DATA_INGESTION_INGESTED_DIR: str = "ingested"
+DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.2
